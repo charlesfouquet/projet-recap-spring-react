d7158e0a45f7dfbf85d82ffbd434548dd7eed1ba
Charles Fouquet <fouquetcharles@gmail.com>
2023-06-01 17:14:14 +0200

    Push du 20230601

--- prsr-back/src/main/java/fr/charlesfouquet/prsrback/controllers/Home.java
+++ prsr-back/src/main/java/fr/charlesfouquet/prsrback/controllers/Home.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class Home {
     @Autowired
     private UserService userService;
@@ -18,15 +19,19 @@ public class Home {
     @PostMapping("/")
     public ResponseEntity<String> ajout(@RequestBody User user) {
         user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
-        if (userService.create(user)) {
-            return new ResponseEntity<String>("L'utilisateur " + user.getPrenom() + " " + user.getNom() + " a bien été créé !", HttpStatus.CREATED);
+        if (userService.mailExists(user)) {
+            return new ResponseEntity<String>("Un compte avec ce mail existe déjà ! Veuillez réessayer", HttpStatus.BAD_REQUEST);
         } else {
-            return new ResponseEntity<String>("Une erreur est survenue", HttpStatus.BAD_REQUEST);
+            if (userService.create(user)) {
+                return new ResponseEntity<String>("L'utilisateur " + user.getPrenom() + " " + user.getNom() + " a bien été créé !", HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<String>("Une erreur est survenue", HttpStatus.BAD_REQUEST);
+            }
         }
     }
 
     @GetMapping("/")
-    public List<User> home(User user) {
+    public List<User> home() {
         return userService.read();
     }
 
@@ -41,9 +46,10 @@ public class Home {
     }
 
     @DeleteMapping("/")
-    public ResponseEntity<String> delete(@RequestBody User user) {
-        if (userService.delete(user)) {
-            return new ResponseEntity<String>("L'utilisateur " + user.getPrenom() + " " + user.getNom() + " a bien été supprimé !", HttpStatus.CREATED);
+    public ResponseEntity<String> delete(@RequestParam("id") int id) {
+        User userToDelete = userService.findById(id);
+        if (userService.delete(userToDelete)) {
+            return new ResponseEntity<String>("L'utilisateur " + userToDelete.getPrenom() + " " + userToDelete.getNom() + " a bien été supprimé !", HttpStatus.CREATED);
         } else {
             return new ResponseEntity<String>("Une erreur est survenue", HttpStatus.BAD_REQUEST);
         }
